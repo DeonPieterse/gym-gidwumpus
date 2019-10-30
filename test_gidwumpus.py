deb7e62ff0dbb48cbbc60de5dccffb827414e8eb
Deon <Gideondeonjacobus@Gmail.com>
2019-10-30 16:22:24 +0200

    Added gitignore for all the code editor and pip install generated files.

--- test_gidwumpus.py
+++ test_gidwumpus.py
@@ -1,19 +1,27 @@
 import gym
 import gym_gidwumpus
+from datetime import date
 
 env = gym.make('gidwumpus-v0')
 
-# remove this
-print(env.observation_space)
-
-for episode in range(20):
+for episode in range(100):
     observation = env.reset()
+    print('Observation RESETED', observation)
     for t in range(100):
-        env.render()
-        print(observation)
+        print('Observation before action: ', observation)
         action = env.action_space.sample()
+        print('Action: ', action)
         observation, reward, done, info = env.step(action)
+        print('Observation after action: ', observation)
+        print('Reward: ', reward)
+        print('Done: ', done)
+        print('Info: ', info)
+        env.render()
+        if reward == 1:
+            print("---------------------------------------------------------------------------------------------------------------------------------------")
+        print("||")
         if done:
             print("Episode finished after {} timesteps".format(t+1))
+            print("Episode ended: {}\n".format(episode+1))
+            print("========================================================================================================================================\n")
             break
-#env.close()
