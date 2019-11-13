f6e4f29481d1c3e230be452ed2a2f3e4d093c77a
Deon <Gideondeonjacobus@Gmail.com>
2019-11-13 20:21:58 +0200

    House keeping

--- gym_gidwumpus/__init__.py
+++ gym_gidwumpus/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='gidwumpus-v1',
-    entry_point='gym_gidwumpus.gidwumpus_env:GidWumpusEnv',
+    entry_point='envs.gidwumpus_env:GidWumpusEnv',
 )
